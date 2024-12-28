b2487192fdb07419ae315e4ba5bd29ddd3b97da7
Raymond Lei(雷菩宇) <ray810815@gmail.com>
2024-12-28 13:29:44 -0500

    Update Raymond.ts

--- Raymond.ts
+++ Raymond.ts
@@ -1,1 +1,11 @@
+type AngleRaymond = {
+  name: string;
+  goodDeeds: number; <--- right
+};
 
+type EvilRaymond = {
+  name: string;
+  evilPlans: number; <----lame
+};
+
+type Raymond = AngleRaymond | EvilRaymond;(I didn't tag)
